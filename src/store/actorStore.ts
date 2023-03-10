8498e298aafc702b4022586569d9435c1552e567
erhu <1574605559@qq.com>
2023-03-11 01:41:50 +0800

    fix: 组件tree渲染成功~

--- src/store/actorStore.ts
+++ src/store/actorStore.ts
@@ -2,7 +2,6 @@ import { proxy } from 'valtio'
 import { nanoid} from "nanoid"
 import { cloneDeep } from 'lodash-es';
 import { Actor, WidgetType } from '../pages/main/components/wiget-list/share/Widget';
-import { findActor, isStringEmpty } from './utils';
 export function find(findId: string):Actor | null{
     function _find(node: Actor):Actor | null{
         if(node.id == findId) return node;
@@ -60,46 +59,46 @@ export const ActorActions = {
             parent.props.children = [instance];
         }
     },
-    insertActor:(sourceId:string, targetId:string)=>{
-        if(isStringEmpty(sourceId) || isStringEmpty(targetId)) return;
+    // insertActor:(sourceId:string, targetId:string)=>{
+    //     if(isStringEmpty(sourceId) || isStringEmpty(targetId)) return;
 
-        // DFS 寻找
-        function find(node: Actor, id:string):Actor | null{
-            if(node.id == id) return node;
-            const children = node.props.children || [];
-            for(let i = 0; i < children.length; i++){
-                const result = find(children[i], id);
-                if(result !== null) return result;
-            }
-            return null;
-        }
+    //     // DFS 寻找
+    //     function find(node: Actor, id:string):Actor | null{
+    //         if(node.id == id) return node;
+    //         const children = node.props.children || [];
+    //         for(let i = 0; i < children.length; i++){
+    //             const result = find(children[i], id);
+    //             if(result !== null) return result;
+    //         }
+    //         return null;
+    //     }
 
-        const sourceActor = find(actorStore.actorsTree, sourceId);
-        const targetActor = find(actorStore.actorsTree, targetId);
-        if(sourceActor == null || targetActor == null) return;
-        const sourceActorParent = sourceActor?.props.parent;
-        const targetActorParent = targetActor?.props.parent;
-
-
-        const sourceIndex = sourceActorParent?.props.children?.findIndex(child => child.id == sourceId);
-        if(sourceIndex == -1 ) return
-        console.log("删除原node：", sourceActor, "插入的位置后一个node:", targetActor);
-        console.log("删除前:", sourceActorParent?.props.children?.map(ch=> ch.id + ch.type))
-        sourceActorParent?.props.children?.splice(sourceIndex!, 1);
-        console.log("删除后:", sourceActorParent?.props.children?.map(ch=> ch.id + ch.type))
-        const targetIndex = targetActorParent?.props.children?.findIndex(child => child.id == targetId);
+    //     const sourceActor = find(actorStore.actorsTree, sourceId);
+    //     const targetActor = find(actorStore.actorsTree, targetId);
+    //     if(sourceActor == null || targetActor == null) return;
+    //     const sourceActorParent = sourceActor?.props.parent;
+    //     const targetActorParent = targetActor?.props.parent;
 
 
-        console.log("插入前：", targetActorParent?.props.children?.map(ch => ch.id + ch.type));
-        console.log("插入的位置:", targetIndex)
-        if( targetIndex == -1 ) return;
-        if( targetIndex == 0){
-            targetActorParent?.props.children?.unshift(sourceActor)
-        }else{
-            targetActorParent?.props.children?.splice(targetIndex! , 0, sourceActor);
-        }
-        console.log("插入后:", targetActorParent?.props.children?.map(ch => ch.id + ch.type))
-    },
+    //     const sourceIndex = sourceActorParent?.props.children?.findIndex(child => child.id == sourceId);
+    //     if(sourceIndex == -1 ) return
+    //     console.log("删除原node：", sourceActor, "插入的位置后一个node:", targetActor);
+    //     console.log("删除前:", sourceActorParent?.props.children?.map(ch=> ch.id + ch.type))
+    //     sourceActorParent?.props.children?.splice(sourceIndex!, 1);
+    //     console.log("删除后:", sourceActorParent?.props.children?.map(ch=> ch.id + ch.type))
+    //     const targetIndex = targetActorParent?.props.children?.findIndex(child => child.id == targetId);
+
+
+    //     console.log("插入前：", targetActorParent?.props.children?.map(ch => ch.id + ch.type));
+    //     console.log("插入的位置:", targetIndex)
+    //     if( targetIndex == -1 ) return;
+    //     if( targetIndex == 0){
+    //         targetActorParent?.props.children?.unshift(sourceActor)
+    //     }else{
+    //         targetActorParent?.props.children?.splice(targetIndex! , 0, sourceActor);
+    //     }
+    //     console.log("插入后:", targetActorParent?.props.children?.map(ch => ch.id + ch.type))
+    // },
     insertActorToPosition:(sourceId: string, targetId: string, targetPos: number)=>{
         const sourceActor = find(sourceId);
         const sourceActorParent = sourceActor?.props.parent;
@@ -112,11 +111,11 @@ export const ActorActions = {
         }
 
     },
-    MoveActorBeChild:(sourceActorId: string, targetActorId:string)=>{
-        // 1. DFS 寻找 instance;
-        const sourceActor = findActor(actorStore.actors, sourceActorId);
-        if(sourceActor == null) return;
-    },
+    // MoveActorBeChild:(sourceActorId: string, targetActorId:string)=>{
+    //     // 1. DFS 寻找 instance;
+    //     const sourceActor = findActor(actorStore.actors, sourceActorId);
+    //     if(sourceActor == null) return;
+    // },
     activeActor: (id: string | null)=>{
         if(id == null || id?.trim() == "") {
         // 设置 rootNode 为激活
@@ -151,7 +150,7 @@ export const ActorActions = {
 
     deleteActor:(id?: string)=>{
         if(id == void 0 || id.trim() == "") return;
-        const deleteTarget = findActor(actorStore.actorsTree, id);
+        const deleteTarget = find(id); 
         const parent = deleteTarget?.props.parent;
         if(parent && parent.props.children){
             const index = parent.props.children.findIndex(child=> child.id == deleteTarget?.id);
